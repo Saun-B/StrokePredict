919e5f58727d509f19ebdb3a1e07e7f262658e96
Saun-B <nguyenquangsang61206@gmail.com>
2025-10-19 21:32:22 +0700

    new

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask_cors import CORS
 from flask import Flask, render_template, request, jsonify
 from Backend.rules import evaluate_risk, get_advice
@@ -22,4 +24,4 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
